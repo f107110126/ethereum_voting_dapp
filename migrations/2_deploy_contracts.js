640e4820bedf90926390658c7f43c695de2f4bb5
Andy.Chen <f107110126@gmail.com>
2019-11-30 22:24:01 +0000

    update migration

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,6 +3,8 @@ const Voting = artifacts.require('Voting');
 module.exports = function (deployer) {
   deployer.deploy(
     Voting,
+    1000,
+    web3.toWei(0.001, 'ether'),
     ['Rama', 'Nick', 'Jose'].map(name => web3.utils.asciiToHex(name)),
     { gas: 350000 }
   );
